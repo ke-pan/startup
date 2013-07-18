bf3e8967f2aea73c6c449d38f70d8690f455d575
ke pan <ke_pan@yeah.net>
2013-07-18 14:38:43 +0000

    update

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send('C, Kiss!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
